d2b32c751c90a1596b4748455cc1f487e88c8cff
LaanAnla <95160853+LaanAnla@users.noreply.github.com>
2024-01-31 11:22:46 +0100

    mise en place de pug

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send('<div style="color: green; font-weight: bold; text-transform: uppercase;">Hello World!</div>');
+app.set("views", path.join(__dirname, "/views/pages"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+	res.render("index", { title: "Hey", message: "Hello there!" });
 });
 
 app.listen(port, () => {
